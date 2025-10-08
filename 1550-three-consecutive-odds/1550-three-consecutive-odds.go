f665bf1faf6df01367114619ae6e03fb72b393e5
Mahir <talktomahir1@gmail.com>
2025-10-08 10:35:24 +0530

    Time: 0 ms (100%), Space: 4.5 MB (10.14%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.go
@@ -0,0 +1,14 @@
+func threeConsecutiveOdds(arr []int) bool {
+    count := 0
+	for _, num := range arr {
+		if num%2 != 0 {
+			count++
+			if count == 3 {
+				return true
+			}
+		} else {
+			count = 0
+		}
+	}
+	return false
+}
